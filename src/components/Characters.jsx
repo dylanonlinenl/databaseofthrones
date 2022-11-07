3843eb7217aaa8b5480c47790b18febfda3e5c8b
StefGeraets <stef.geraets@gmail.com>
2022-11-07 22:07:58 +0100

    Small refactor

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,66 +1,58 @@
 import {useEffect, useState} from "react";
+import Character from "./Character";
+import { CHARACTER_URL } from "../constants";
 
 function Characters() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null); // Object type. {key: string, value: [string]}
+    const [isLoaded, setIsLoaded] = useState(true);
     const [characters, setCharacters] = useState({});
     const [characterId, setCharacterId] = useState(null);
 
-    function eventCallback(event) {
-        setCharacterId(event.target.id);
-    }
-
     useEffect(() => {
         let fetchId;
-        characterId ? fetchId = fetch(`https://thronesapi.com/api/v2/Characters/${characterId}`) : fetchId = fetch("https://thronesapi.com/api/v2/Characters");
+        
+        // Array with character objects
+        fetchId = fetch(`${CHARACTER_URL}`);
+        
+        // console.log("Use", characterId, characters);
         fetchId.then((res) => res.json())
             .then(
                 (result) => {
-                    setIsLoaded(true);
+                    setIsLoaded(false);
                     setCharacters(result);
                 },
                 (error) => {
-                    setIsLoaded(true);
+                    setIsLoaded(false);
                     setError(error);
                 }
             );
     }, [characterId]);
 
-    if (!isLoaded) {
-        return <div className="loading-message"><h1>Loading...</h1></div>;
-    } else if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (Object.values(characters)[0] === parseInt(characterId)) {
-        let character = characters;
-        return (
-            <div id="character_detail">
-                <div className="character-block">
-                    <img src={character.imageUrl} alt={character.fullName}></img>
-                    <h1>{character.fullName}</h1>
-                    <h2>{character.title}</h2>
-                </div>
-                {/*Todo: fix going back to character grid after click on the div down here*/}
-                <div className="character-return" onClick={() => setCharacterId(null)}>
-                    <h3>Return to all characters</h3>
-                </div>
-            </div>
-        );
-    } else {
-        return (
-            <div id="character_grid">
-                {characters.map((character) => {
-                    return (
-                        <div className="character-block">
-                            <img src={character.imageUrl} alt={character.fullName} id={character.id}
-                                 onClick={eventCallback}></img>
-                            <h1>{character.fullName}</h1>
-                            <h2>{character.title}</h2>
-                        </div>
-                    );
-                })}
-            </div>
-        );
+    if (isLoaded) return <div className="loading-message"><h1>Loading...</h1></div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+
+    if (characterId) { 
+        return (<Character characterId={characterId} />)
     }
+    
+    return (
+        <div id="character_grid">
+            {characters.map((character) => {
+                return (
+                    <div className="character-block" key={character.id}>
+                        <img src={character.imageUrl} alt={character.fullName}
+                                onClick={() => {
+                                setCharacterId(character.id)
+                            }}></img>
+                        <h1>{character.fullName}</h1>
+                        <h2>{character.title}</h2>
+                    </div>
+                );
+            })}
+        </div>
+    );
+
 }
 
 export default Characters;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const CHARACTER_URL = 'https://thronesapi.com/api/v2/Characters'
